7cdd2c0b08c465115f65ff76c3a0524dbc67ab3d
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-06 14:24:24 -0400

    Create restaurants table

--- db/seed.cjs
+++ db/seed.cjs
@@ -1,8 +1,39 @@
 const client = require('./client.cjs');
 
+const dropTables = async() => {
+  try {
+    await client.query(`DROP TABLE IF EXISTS restaurants;`)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const createTables = async() => {
+  try {
+    await client.query(`
+      CREATE TABLE restaurants (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        cuisine_type VARCHAR(30) NOT NULL 
+      );
+    `)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 const syncAndSeed = async() => {
   await client.connect();
   console.log(`connected`);
+
+  await dropTables();
+  console.log(`dropped tables`)
+
+  await createTables();
+  console.log(`created tables`);
+
+  await client.end();
+  console.log(`disconnected`);
 }
 
 syncAndSeed();
