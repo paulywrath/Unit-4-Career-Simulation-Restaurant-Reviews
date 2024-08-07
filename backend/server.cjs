9a145f5e844e4dce1cc5d19b7f53997028ac37e0
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-06 21:26:55 -0400

    Initial commit

---
+++ backend/server.cjs
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
